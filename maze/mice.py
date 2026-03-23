8d69689a96d5e23ea6bea21f72ee6c08e42611fd
Stepan Blinov <blinov.school@yandex.ru>
2026-03-23 14:05:13 +0300

    Mouse AI done

--- maze/mice.py
+++ maze/mice.py
@@ -21,16 +21,6 @@ class Mouse:
 
 # немного интеллекта
 class Mouse2(Mouse):
-    def __init__(self, x, y, dir=0):
-        super().__init__(x, y, dir)
-        self.x, self.y = x, y
-        self.size = 1 / 20  # доля тайла, тайлы 1x1
-        self.speed = 1  # тайлов в секунду
-        self.dir = dir
-
-    def draw(self):
-        graphics.draw_circle("yellow", self.x, self.y, self.size)
-
     def update(self, delta_time):
         cur_tile = Maze.get_tile(self.x, self.y)
         dx, dy = directions[self.dir]
@@ -39,7 +29,7 @@ class Mouse2(Mouse):
         next_tile = cur_tile.get_neighb_tile(self.dir)
         if cur_tile.dist_to_border(self.x, self.y, self.dir) < 0.2 and (
                 next_tile is None or isinstance(next_tile, Wall_tile)):
-            self.dir = (self.dir + 1) % 4
+            self.dir = (self.dir - 1) % 4
 
 
 
