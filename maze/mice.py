7a2d9831d63a495eebef70df9fd0e160655e4f90
Stepan Blinov <blinov.school@yandex.ru>
2026-03-24 07:06:11 +0300

    DFS mouse done

--- maze/mice.py
+++ maze/mice.py
@@ -32,7 +32,55 @@ class Mouse2(Mouse):
             self.dir = (self.dir - 1) % 4
 
 
+class SmartMouse(Mouse):
+    def __init__(self, x, y, dir=0):
+        super().__init__(x, y, dir)
+        self.target = None
+        self.path = None
+        self.next = Maze.get_tile(self.x, self.y)
 
 
+    def update(self, delta_time):
+        if self.path is None:
+            return
+        cur_tile = Maze.get_tile(self.x, self.y)
+        if cur_tile.column == self.target[0] and cur_tile.row == self.target[1]:
+            self.path = None
+        dx, dy = directions[self.dir]
+        self.x += dx * self.speed * delta_time
+        self.y += dy * self.speed * delta_time
+        if cur_tile == self.next and cur_tile.dist_to_border(self.x, self.y, self.dir) < 0.5:
+            self.dir = next(self.path, self.dir)
+            self.next = cur_tile.get_neighb_tile(self.dir)
+    
 
+    def __find_path(self):
+        if self.target is None:
+            return
+        
+        visited = set()
+        def dfs(tile):
+            visited.add(tile)
+            if tile.column == self.target[0] and tile.row == self.target[1]:
+                return []
+            for dir in range(len(directions)):
+                next_tile = tile.get_neighb_tile(dir)
+                if isinstance(next_tile, Room_tile) and next_tile not in visited:
+                    res = dfs(next_tile)
+                    if res is not None:
+                        res.append(dir)
+                        return res
+            return None
+        
+        print("Starting DFS...")
+        rev_path = dfs(Maze.get_tile(self.x, self.y))
+        print(f"DFS done. Result: {rev_path}")
+        if rev_path is None:
+            self.path = rev_path
+        else:
+            self.path = reversed(rev_path)
+    
 
+    def goto_cheese(self, x_cheese, y_cheese):
+        self.target = (x_cheese, y_cheese)
+        self.__find_path()
